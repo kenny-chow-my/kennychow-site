ef14480fbc751a364ae8f25e170b8d3db5e31034
Kenny Chow <kenny.chow@grabtaxi.com>
2023-01-26 11:10:07 +0800

    change to axios

--- netlify/functions/webhook2email/webhook2email.js
+++ netlify/functions/webhook2email/webhook2email.js
@@ -1,36 +1,38 @@
-const mailgun = require('mailgun-js');
-
-
+const querystring = require('querystring');
+const axios = require('axios');
 
 exports.handler = async (event, context) => {
-    const mg = mailgun({
-        apiKey: process.env.mgapikey, 
-        domain: "sandboxdf9da5ee163d428f84539d4f489e1398.mailgun.org"
-    });
+
+    const apiKey = process.env.mgapikey;
+    const domain = "sandboxdf9da5ee163d428f84539d4f489e1398.mailgun.org";
 
     const data = {
-    from: 'PostMaster <' + process.env.from + '>',
-    to: process.env.toemail,
-    subject: 'SUBJECT',
-    text: JSON.stringify(event)
+        from: process.env.from,
+        to: process.env.toemail,
+        subject: 'Webhook Received',
+        text: JSON.stringify(event)
     };
 
-   const resp = await mg.messages().send(data, (error, body) => 
-   {
-        if (error)
-        {
-            return {
-                statusCode: 501,
-                body: JSON.stringify(error),
-            };
-        }
-        else{
-            return {
-                statusCode: 200,
-                body: JSON.stringify({ message: "Sent to " + data.to }),
-              };
-        }
-   });
-   console.log(resp);
-   return resp;
+    const resp = await axios({
+            method: 'POST',
+            url: `https://api.mailgun.net/v3/${domain}/messages`,
+            auth: { username: "api", password: apiKey },
+            data: querystring.stringify(data),
+            headers: {'content-type': 'application/x-www-form-urlencoded;charset=utf-8'}
+        }).catch(function (err) {
+            return err;
+        });
+
+    if (err instanceof Error) {
+        return {
+            statusCode: 501,
+            body: JSON.stringify(error),
+        };
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Sent to " + data.to }),
+        };
+
   };
